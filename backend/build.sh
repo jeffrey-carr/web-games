56928ecb6f0d199cfd8e423412696d0ba6961d99
Jeffrey Carr <jeffrey.carr98@gmail.com>
2025-03-16 14:35:08 -0400

    idk

--- backend/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Building project..."
-go build -o output.out "./"
-echo "Project Built!"
---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import '$lib/globals.css';
+
+	let { children }: { children: () => any } = $props();
+</script>
+
+{@render children()}
